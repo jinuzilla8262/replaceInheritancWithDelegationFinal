aac8da5f2c301cf098e4d4c2236ffd73ee65b76f
jinuz <jinuzilla@gmail.com>
2024-03-06 19:48:01 -0500

    Created test case for push() pop() Test Results: Passed

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,14 +1,39 @@
 package org.example;
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
+import java.util.EmptyStackException;
 /**
  * Unit test for simple App.
  */
 public class AppTest
-    extends TestCase
+        extends TestCase
 {
-
+    public void testPush() {
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(1);
+        stack.push(2);
+        assertEquals(2, stack.size());
+        assertEquals(Integer.valueOf(2), stack.get(stack.size() - 1));
+        assertEquals(Integer.valueOf(2), stack.pop());
+        assertEquals(Integer.valueOf(1), stack.pop());
+        assertTrue(stack.isEmpty());
+    }
+    public void testPop() {
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(1);
+        stack.push(2);
+        assertEquals(Integer.valueOf(2), stack.pop());
+        assertEquals(Integer.valueOf(1), stack.pop());
+        assertTrue(stack.isEmpty());
+    }
+    public void testPopEmptyStack() {
+        MyStack<Integer> stack = new MyStack<>();
+        try {
+            stack.pop();
+            fail("Expected EmptyStackException");
+        } catch (EmptyStackException e) {
+            // Expected
+        }
+    }
 }
